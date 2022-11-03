b013276e98f7e0c72b95d6cccf0b25bffef14339
EwanColquhoun <ewancolquhoun@hotmail.com>
2022-11-03 22:04:04 +0000

    Add basic styling p2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function calculateDays(days){
     results.innerHTML = `
     <span>Your total hire is ${days} days.<br>
     Total estimate is £${quote}</span>`;
-    console.log('calculateDays')
+    return days
 }
 
 function calculateQuote(days, price){
@@ -30,20 +30,25 @@ function discount(days){
 const picker = new easepick.create({
     element: document.getElementById('datepicker'),
     css: [
-      'https://cdn.jsdelivr.net/npm/@easepick/core@1.2.0/dist/index.css',
+      'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.0/dist/index.css',
       'https://cdn.jsdelivr.net/npm/@easepick/range-plugin@1.2.0/dist/index.css',
     ],
+    autoApply: false,
     plugins: ['RangePlugin'],
     RangePlugin: {
       tooltipNumber(num) {
-        console.log(num-1);
         let days = num-1;
-        calculateDays(days)
-        return days;
+        return calculateDays(days);
       },
       locale: {
         one: 'night',
         other: 'nights',
-      }, tooltip: true,
+      }, 
     },
+    setup(picker) {
+      picker.on('select', (e) => {
+      //    let startDate =  picker.getStartDate('DD MMM YYYY');
+        //  document.getElementById('test').innerHTML = 'Start date: '+startDate+' End date: '+endDate;
+      });
+   },
   });
